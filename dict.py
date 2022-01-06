0eed517be02ba1c778bf837e8c0baab3d82ecacd
HanDiepp <maithanhdat052@gmail.com>
2022-01-06 13:23:51 +0700

    a1

---
+++ dict.py
@@ -0,0 +1,11 @@
+anh_viet = {
+	"milk": "sua",
+	"rice": "gao",
+	"fish": "ca",
+	"banana": "chuoi"
+}
+# x = int(input("Nhap x: "))
+if "sua" in anh_viet.get("rice"):
+	print("Co")
+else:
+	print("Khong")
